6a55fc8d5f4c62204acee28e58f5caf6d8cee961
Moharram Moharrami <moharram.m98@gmail.com>
2021-12-07 18:41:26 +0330

    make play button plays the audio

--- day7/index.js
+++ day7/index.js
@@ -1,3 +1,8 @@
 const playBtn = document.getElementById("play-btn")
 const pauseBtn = document.getElementById("pause-btn")
 const stopBtn = document.getElementById("stop-btn")
+
+var audioElement = new Audio('bells.mp3')
+playBtn.addEventListener("click", function() {
+    audioElement.play();
+})
