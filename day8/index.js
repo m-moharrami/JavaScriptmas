630bf57619099ca5403ebe53b0b36b26cb52511e
Moharram Moharrami <moharram.m98@gmail.com>
2021-12-08 17:07:28 +0330

    make the bell shake and pause with the music play the pause

--- day8/index.js
+++ day8/index.js
@@ -4,14 +4,18 @@ const stopBtn = document.getElementById("stop-btn")
 var audioElement = new Audio('bells.mp3')
 
 playBtn.addEventListener("click", function() {
+    bell.classList.add("shaking");
+    bell.style.animationPlayState = 'running';
     audioElement.play();
 })
 
 pauseBtn.addEventListener("click", function() {
+    bell.style.animationPlayState = 'paused';
     audioElement.pause();
 })
 
 stopBtn.addEventListener("click", function() {
+    bell.classList.remove("shaking");
     audioElement.pause();
     audioElement.currentTime = 0;
 })
