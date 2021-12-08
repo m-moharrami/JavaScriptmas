07a39974f0c793aa7c3cfe10ccae250927214349
Moharram Moharrami <moharram.m98@gmail.com>
2021-12-08 14:10:26 +0330

    just copy yesterdays challenge for now

---
+++ day8/index.js
@@ -0,0 +1,17 @@
+const playBtn = document.getElementById("play-btn")
+const pauseBtn = document.getElementById("pause-btn")
+const stopBtn = document.getElementById("stop-btn")
+var audioElement = new Audio('bells.mp3')
+
+playBtn.addEventListener("click", function() {
+    audioElement.play();
+})
+
+pauseBtn.addEventListener("click", function() {
+    audioElement.pause();
+})
+
+stopBtn.addEventListener("click", function() {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+})
